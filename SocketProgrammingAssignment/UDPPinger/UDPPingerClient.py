07e3db1a38ef1773932092d1b5e514b37ad90c29
fanqiechaodan <1484119655@qq.com>
2024-07-19 11:38:15 +0800

    Modifiy UDPPingerClient.py

--- SocketProgrammingAssignment/UDPPinger/UDPPingerClient.py
+++ SocketProgrammingAssignment/UDPPinger/UDPPingerClient.py
@@ -3,6 +3,11 @@ import time
 import argparse
 
 def repeat_ten_ping(ip, port):
+    total_rtt = 0
+    max_rtt = 0
+    min_rtt = 1000
+    receive_packets = 0
+
     clientSocket = socket(AF_INET, SOCK_DGRAM)
     clientSocket.settimeout(1)
     for i in range(1, 11):
@@ -11,10 +16,20 @@ def repeat_ten_ping(ip, port):
         clientSocket.sendto(message.encode(), (ip, port))
         try:
             modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-            recv_time = time.time()
-            print(f'Response from {serverAddress}: sequence={i} length={len(modifiedMessage)} bytes RTT={(recv_time-send_time)*100:{4}.{2}}ms')
+            rtt = (time.time() - send_time)*1000
+
+            total_rtt += rtt
+            max_rtt = max(max_rtt, rtt)
+            min_rtt = min(min_rtt, rtt)
+            receive_packets += 1
+
+            print(f'Response from {serverAddress}: sequence={i} length={len(modifiedMessage)}bytes RTT={rtt:.2f}ms')
         except TimeoutError:
             print('Request timed out')
+    
+    print(f'--- {ip} ping statistics ---')
+    print(f'10 packets transmitted, {receive_packets} received, {(10-receive_packets)/10*100:.2f}% packet loss, time {total_rtt:.3f}ms')
+    print(f'rtt min/avg/max = {min_rtt:.3f}/{total_rtt/10:.3f}/{max_rtt:.3f} ms')
     
     clientSocket.close()
     
